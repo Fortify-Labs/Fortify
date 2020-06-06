3a64f1cfc8cbafbfbf522cad54a21659b92aa88e
Thomas Kosiewski <thoma471@googlemail.com>
2020-06-07 01:54:07 +0200

    Added cdk8s

---
+++ devops/kubernetes/main.ts
@@ -0,0 +1,15 @@
+import { Construct } from 'constructs';
+import { App, Chart } from 'cdk8s';
+
+export class MyChart extends Chart {
+  constructor(scope: Construct, name: string) {
+    super(scope, name);
+
+    // define resources here
+
+  }
+}
+
+const app = new App();
+new MyChart(app, 'kubernetes');
+app.synth();
