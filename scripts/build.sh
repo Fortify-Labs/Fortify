d02ab46822e021c835689a5694850ff0d7bcf6eb
Thomas Kosiewski <thoma471@googlemail.com>
2020-07-11 13:30:47 +0200

    Build script now only building missing images after pull

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,6 +8,18 @@ export FSM_VERSION=`cd services/fsm && node -p "require('./package.json').versio
 export GSI_RECEIVER_VERSION=`cd services/gsi-receiver && node -p "require('./package.json').version"`
 export TWITCHBOT_VERSION=`cd services/17kmmrbot && node -p "require('./package.json').version"`
 
-docker-compose -f build.docker-compose.yml pull
-docker-compose -f build.docker-compose.yml build --parallel
-docker-compose -f build.docker-compose.yml push
+# Fetch the pull output telling
+export DC_PULL=$(docker-compose -f build.docker-compose.yml pull --ignore-pull-failures 2>&1)
+
+# Extract the last line, telling which images need to be rebuild
+export DC_BUILD=$(echo "${DC_PULL}" | tail -n1 | xargs)
+export DC_BUILD=$(echo "${DC_BUILD/docker-compose/docker-compose -f build.docker-compose.yml}")
+echo $DC_BUILD
+
+# If that line starts with docker compose, we will build and push new images
+if  [[ $DC_BUILD == docker-compose* ]]  ;
+then
+    eval $DC_BUILD
+
+	docker-compose -f build.docker-compose.yml push
+fi
