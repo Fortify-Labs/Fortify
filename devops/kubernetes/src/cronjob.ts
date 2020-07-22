939d6b34b8970cd7a2769f1a267b994d1a148d56
Thomas Kosiewski <thoma471@googlemail.com>
2020-07-22 13:38:30 +0200

    Create CronJob constructs in cdk8s Fixes #31

---
+++ devops/kubernetes/src/cronjob.ts
@@ -0,0 +1,86 @@
+import { Construct } from "constructs";
+import { CronJob, EnvVar } from "../imports/k8s";
+
+const { REGISTRY } = process.env;
+
+export interface FortifyCronJobOptions {
+	readonly schedule: string;
+	readonly script: string;
+
+	readonly name: string;
+	readonly version: string;
+
+	// absolute image name
+	readonly image?: string;
+	// only the image name, registry and version will be added
+	// will only be used when image variable is not set
+	readonly imageName?: string;
+
+	readonly env?: EnvVar[] | undefined;
+	readonly configmaps?: string[];
+	readonly secrets?: string[];
+}
+
+export class FortifyCronJob extends Construct {
+	constructor(scope: Construct, ns: string, options: FortifyCronJobOptions) {
+		super(scope, ns);
+
+		const configmaps = options.configmaps ?? [];
+		const secrets = options.secrets ?? [];
+		const env: EnvVar[] = options.env
+			? [...options.env, { name: "DEBUG", value: "app::*" }]
+			: [];
+
+		const image =
+			options.image ??
+			(REGISTRY ?? "") +
+				(options.imageName ?? "jobs") +
+				":" +
+				(options.version ?? "invalid");
+
+		new CronJob(this, "cronjob", {
+			metadata: {
+				name: options.name,
+			},
+			spec: {
+				schedule: options.schedule,
+				jobTemplate: {
+					spec: {
+						template: {
+							spec: {
+								containers: [
+									{
+										name: options.name,
+										image,
+										env,
+										envFrom: [
+											...configmaps.map((name) => ({
+												configMapRef: {
+													name,
+												},
+											})),
+											...secrets.map((name) => ({
+												secretRef: {
+													name,
+												},
+											})),
+										],
+										command: [
+											"npm",
+											"run",
+											"start",
+											"--",
+											"run",
+											options.script,
+										],
+									},
+								],
+								restartPolicy: "OnFailure",
+							},
+						},
+					},
+				},
+			},
+		});
+	}
+}
