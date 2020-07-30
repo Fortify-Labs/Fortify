223dda6d38cdd74fc077e44a85a97ebe20172063
Thomas Kosiewski <thoma471@googlemail.com>
2020-07-30 15:36:57 +0200

    Version bumps

--- scripts/build.sh
+++ scripts/build.sh
@@ -14,15 +14,19 @@ export JOBS_VERSION=`cd services/jobs && node -p "require('./package.json').vers
 # Build the base image
 export DC_PULL_BASE=$(docker-compose -f build.docker-compose.yml pull --ignore-pull-failures base 2>&1)
 echo -n "$DC_PULL_BASE"
+echo ""
 export DC_BUILD_BASE=$(docker-compose -f build.docker-compose.yml build base 2>&1)
 echo -n "$DC_BUILD_BASE"
+echo ""
 export DC_PUSH_BASE=$(docker-compose -f build.docker-compose.yml push base 2>&1)
 echo -n "$DC_PUSH_BASE"
+echo ""
 
 # Fetch the pull output
 echo "docker-compose -f build.docker-compose.yml pull --ignore-pull-failures"
 export DC_PULL=$(docker-compose -f build.docker-compose.yml pull --ignore-pull-failures 2>&1)
 echo -n "$DC_PULL"
+echo ""
 
 # Extract the last line, telling which images need to be rebuild
 export DC_BUILD=$(echo -n "${DC_PULL}" | tail -n 1 | xargs)
@@ -36,12 +40,15 @@ export DC_BUILD=$(echo -n "${DC_BUILD/docker-compose/$REPLACE_STRING}")
 # If that line starts with docker compose, we will build and push new images
 if case $DC_BUILD in "docker-compose"*) true;; *) false;; esac; then
 	echo -n "$DC_BUILD"
+	echo ""
 
   	export DC_BUILD_OUTPUT=$(eval $DC_BUILD 2>&1)
 	echo -n "$DC_BUILD_OUTPUT"
+	echo ""
 
 	export DC_PUSH_OUTPUT=$(docker-compose -f build.docker-compose.yml push 2>&1)
 	echo -n "$DC_PUSH_OUTPUT"
+	echo ""
 else
   echo "No new images to build"
 fi
