bd9e46535c8f627456df858028a778cb8820ca81
Thomas Kosiewski <thoma471@googlemail.com>
2020-08-15 20:10:54 +0200

    Add cookie consent box Fixes #108

--- services/frontend/pages/_app.tsx
+++ services/frontend/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from "next/app";
 
 import { Footer } from "../components/footer";
+import CookieConsent from "react-cookie-consent";
 
 import "../sass/mystyles.scss";
 import styles from "../css/_appStyles.module.css";
@@ -12,6 +13,19 @@ function MyApp({ Component, pageProps }: AppProps) {
 				<Component {...pageProps} />
 			</div>
 			<Footer />
+
+			<CookieConsent
+				location="bottom"
+				enableDeclineButton
+				sameSite="strict"
+				overlay={true}
+				setDeclineCookie={false}
+				onDecline={() =>
+					(document.location.href = "https://google.com")
+				}
+			>
+				This website uses cookies to enhance the user experience.
+			</CookieConsent>
 		</div>
 	);
 }
